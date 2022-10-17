1e89b01bd58e24ce86175fd4e50cba1e829a0bd0
Sanian <76259603+Sanian-Creations@users.noreply.github.com>
2022-10-17 14:10:50 +0200

    chrome support

--- sysp.user.js
+++ sysp.user.js
@@ -3,44 +3,27 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.youtube.com/embed/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Sanian
-// @run-at      document-start
+// @run-at      document-end
 // @description Screw you sandy pall, suck my nuts. (Don't actually, you're gross. Its a figure of speech because I despise you)
 // ==/UserScript==
 
-const banned_channels = ["UCWLrDy9B9k0GMS1n-wziQBA"]; // channel IDs go here
-const exp = new RegExp(banned_channels.join("|"));
+const banned_channels = ["UCWLrDy9B9k0GMS1n-wziQBA"];  // Channel IDs go here
+const reg_exp = new RegExp(banned_channels.join("|")); // Regular expression containing all banned channel IDs
 
-let BANNED = false;
+new MutationObserver(check_channel).observe(document, {subtree: true, childList: true});
 
-// This event only works on firefox
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/beforescriptexecute_event
-window.addEventListener('beforescriptexecute', check_channel);
-
-function check_channel(e) {
-
-  if (BANNED) { // stop other scripts once we've already determined this vid is banned.
-    e.preventDefault();
-    console.log("stopped script of a banned channel");
-    return;
-  }
-
+function check_channel(mutation_record, observer) {
   // Channel logo alows us to identify the channel, if it is not on the page then we don't know what to do yet.
   const channel_logo = document.querySelector(".ytp-title-channel-logo");
   if (!channel_logo) return;
 
-  // Channel logo contains a link to the channel, check if that link contains the banned string
-  const banned = exp.test(channel_logo.href);
+  const banned = reg_exp.test(channel_logo.href); // See if channel logo link contains a banned channel ID
   if (!banned) { // Not banned, this vid is fine.
     window.removeEventListener('beforescriptexecute', check_channel);
     return;
   }
-
-  BANNED = true;
-
-  // Prevent this script from executing further
-  e.preventDefault();
 
   // Stop any resource loading. Very important. If you dont do this, the video (despite being
   // removed from the body in the code below) will still load and its audio will start playing.
@@ -48,10 +31,13 @@ function check_channel(e) {
 
   // Replace the embedded video with a little message explaining why it is gone.
   const html = document.body.parentElement;
-  document.body.remove();
+  document.body.remove(); // remove everything. (don't clear out all content of the html tag, for some reason that triggers code which puts the body back)
+  html.style.background = "black";
+
   const msg = document.createElement("p");
   msg.style.color = "white";
   msg.textContent = "Banned YouTube channel";
   html.append(msg);
-  html.style.background = "black";
+
+  observer.disconnect();
 }
